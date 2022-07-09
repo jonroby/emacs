8d1b67dbf40f6acbb4f6d182eaa841dba2e4c717
jonroby <jonathanroby@protonmail.com>
2022-07-09 08:08:02 -0700

    handle most appearance settings

--- init.el
+++ init.el
@@ -11,4 +11,34 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-(straight-use-package 'evil)
+(setq org-src-preserve-indentation t)
+
+(setq ring-bell-function 'ignore)
+
+(setq auto-save-default nil)
+
+(setq make-backup-files nil)
+
+(setq create-lockfiles nil)
+
+(straight-use-package
+  '(nano-emacs :type git :host github :repo "rougier/nano-emacs"))
+
+(require 'nano)
+(require 'nano-theme-dark)
+
+(require 'nano-faces)
+(nano-faces)
+
+(require 'nano-theme)
+(nano-theme)
+
+(set-face-attribute 'default nil
+                    :family "SF Mono"
+                    :height 130
+                    :weight 'normal
+                    :width 'normal))
+
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+
+(setq-default line-spacing 2)
