52469bcf1fa7a9db4262dc9cc09bc68578d0dac2
jonroby <jonathanroby@protonmail.com>
2022-09-18 17:11:16 -0500

    update straight packages

--- init.el
+++ init.el
@@ -416,3 +416,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(helm-M-x-key ((t (:extend t :foreground "#434C5E")))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-minibuffer-history-key "M-p")
+ '(magit-commit-show-diff nil))
